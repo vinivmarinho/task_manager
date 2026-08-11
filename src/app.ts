803989f2a43568d11a6c9d9496601fd0ba71e6f4
Vinícius Marinho <vini.marinho2004@gmail.com>
2026-08-11 12:25:43 -0400

    feat: Adiciona conexão ao postgresql e apaga rotas e controllers que usavam array para armazenar os dados

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 import express, {type Express, type Request, type Response } from "express";
-import userRouter from "./routes/user.routes";
 const app: Express = express();
 
 app.use(express.json());
@@ -9,7 +8,5 @@ app.get("/", (req: Request, res: Response) => {
     console.log("Rota teste funcionando")
 });
 
-// Rotas de usuário
-app.use("/users", userRouter);
 
 export default app;
--- src/routes/user.routes.ts
+++
@@ -1,8 +0,0 @@
-import Router from "express";
-import { createUser, showUsers } from "../controllers/users.controller";
-const router = Router();
-
-router.post("/", createUser);
-router.get("/", showUsers);
-
-export default router;
