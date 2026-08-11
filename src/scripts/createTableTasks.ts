d4f5a128ddd54706cd357e70f26ea78b904526b5
Vinícius Marinho <vini.marinho2004@gmail.com>
2026-08-11 15:10:29 -0400

    feat: Adiciona script para criar tabela de tasks

---
+++ src/scripts/createTableTasks.ts
@@ -0,0 +1,22 @@
+import pool  from "../config/database";
+
+async function createTableTasks() {
+    try {
+        await pool.query(`
+        CREATE TABLE tasks(
+        task_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        task_name VARCHAR(50) NOT NULL,
+        task_description text,
+        task_priority VARCHAR(20) CHECK(task_priority IN('Alta', 'Média', 'Baixa')),
+        user_id INT NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES users(user_id)
+        )
+        `);
+        console.log('Tabela tasks criada com sucesso')
+    } catch(error) {
+        console.log(`Erro ao criar tabela tasks: ${error}`)
+    } finally {
+        await pool.end()
+    }
+};
+createTableTasks();
