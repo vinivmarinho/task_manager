5a9dc1a8c55be48204543fb4ef133235b2147a41
Vinícius Marinho <vini.marinho2004@gmail.com>
2026-08-12 11:19:11 -0400

    feat: Adiciona script para povoar a tabela tasks

---
+++ src/scripts/seedRandomTasks.ts
@@ -0,0 +1,34 @@
+import pool from "../config/database";
+import { faker } from "@faker-js/faker";
+
+async function seedRandomTasks() {
+    try {
+        const result = await pool.query("SELECT user_id FROM users");
+        const users = result.rows;
+
+  
+        if (users.length == 0) {
+            console.log("Nenhum usuário encontrado")
+            return
+        };
+
+        for (let i = 0; i < 100; i++) {
+            const randomUser = users[Math.floor(Math.random() * users.length)];
+
+            const name = faker.lorem.words(3);
+            const description = faker.lorem.sentence();
+            const priority = faker.helpers.arrayElement(["Alta", "Média", "Baixa"]);
+
+            await pool.query(`
+                INSERT INTO tasks(task_name, task_description, task_priority, user_id) VALUES ($1, $2, $3, $4)`, [name, description, priority, randomUser.user_id ])
+        }
+              
+        console.log("100 tasks criadas com sucesso");
+    } catch(error) {
+        console.log(`Não foi possível criar as tasks, erro: ${error}`)
+    } finally {
+        await pool.end();
+    }
+}
+
+seedRandomTasks();
